685a3b93a433913cd05d7b3f5111c127b9df1b60
protapchakma <protapchakma43@gmail.com>
2026-04-24 17:01:34 +0600

    all file and folder added by 1st commit

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+         
+        Hi! I'm PROTAP CHAKMA Frontend Developer.
+                i am a Computer Science Engineering student at Chattogram Polytechnic Institute, currently studying in the 4th semester. My home district is Khagrachhari, Chattogram, Bangladesh. I am motivated to learn new technologies and improve my technical skills, i have basic knowledge of HTML, CSS, java script,python,java and computer applications. I am seeking opportunities to gain practical experience and grow professionally in the IT field.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,73 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:chakmaprotap855@gmail.com.com" data-cursor="disable">
+                chakmaprotap855@gmail.com.com
+              </a>
+            </p>
+            <h4>Phone</h4>
+            <p>
+              <a href="tel:01533098947" data-cursor="disable">
+                +880 1533 098947
+              </a>
+            </p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/coderprotap67"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/protap-chakma-0b2a6b24a/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Protap Chakma</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026 Protap Chakma. All rights reserved.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
